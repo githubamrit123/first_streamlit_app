9c26a368e04dad4f26c3d85d8c6fcd81c418c5d9
Amrit Singh <116111765+githubamrit123@users.noreply.github.com>
2022-10-19 00:54:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.title('My parents new healthy dinner')
-st.header("""# Breakfast menu
-         omega 3 and Blueberry Oatmeal""")
+st.header(" Breakfast menu")
+st.text("omega 3 and Blueberry Oatmeal")
