841cd4691fc8e5347c299dd1b5e0bd599eeb754d
Amrit Singh <116111765+githubamrit123@users.noreply.github.com>
2022-10-30 01:26:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?')
 if not fruit_choice:
-  streamlit.error("please")
-  else:
+streamlit.error("please")
+else:
 #streamlit.write('The user entered ', fruit_choice)
 #import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
